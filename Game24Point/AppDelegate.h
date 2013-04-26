c897816a836111cd285aff8b84d93467c367a147
kingiol <kingxiaokang@gmail.com>
2013-04-26 20:58:57 +0800

    add project file

---
+++ Game24Point/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Game24Point
+//
+//  Created by Kingiol on 13-4-26.
+//  Copyright (c) 2013年 Kingiol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Game24Point/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Game24Point
+//
+//  Created by Kingiol on 13-4-26.
+//  Copyright (c) 2013年 Kingiol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
